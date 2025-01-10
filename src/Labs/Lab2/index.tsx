3dc24aa74d97640a77a1d7750838a7f307b10517
tanmaydemble <tanmay12demble@gmail.com>
2025-01-10 15:47:37 -0500

    removed unused react import

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Lab2() {
     console.log("Rendering Lab1 Component");
     return <h2>Lab 2</h2>;
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Lab3() {
     console.log("Rendering Lab1 Component");
     return <h2>Lab 3</h2>;
